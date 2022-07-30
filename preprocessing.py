fd428dec57a8b7782d2ce1ffd6ef829ef1a33565
WU Junyan <85872793+SupercarryNg@users.noreply.github.com>
2022-07-30 10:24:25 +0800

    Add files via upload

--- preprocessing.py
+++ preprocessing.py
@@ -1,56 +1,59 @@
 import re
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import *
+
 import pandas as pd
+import numpy as np
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 
 class Preprocessing:
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, positive_num):
         self.df = pd.read_csv(csv_file)
         self.df = self.get_sentiment()
 
+        self.get_sample(positive_num=positive_num)
+
     @staticmethod
     def rem_meaningless(tweet):
-        # remove the urls
         tweet = re.sub(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", tweet)
-        # remove @ and references
         tweet = re.sub(r"(@[A-Za-z0-9_]+)", "", tweet)
-        # remove special charaecters
         tweet = re.sub(r"[#:)\n/_\-&]", "", tweet)
         return tweet
 
     @staticmethod
     def stemming(filtered):
-        # return each word's token
         stemmer = PorterStemmer()
         cleaned = [stemmer.stem(word) for word in filtered]
         return cleaned
 
     def get_sentiment(self):
-        # label 1,2 --> negative; label 3 --> neutral; label 4,5 --> positive;
-        self.df.dropna(axis = 0, inplace = True)
+        self.df.dropna(axis=0, inplace=True)
         self.df['sentiment'] = 0
-        dic = {1:'negative',2:'negative',3:'neutral',4:'positive',5:'positive',}
-        self.df['sentiment'] = self.df['Label'].map(lambda x:dic[x])
+        # map func?
+        dict_ = {1: 'negative', 2: 'negative', 3: 'neutral', 4: 'positive', 5: 'positive', }
+        self.df['sentiment'] = self.df['Label'].map(lambda x: dict_[x])
         return self.df
-    
-    def getsample(self, positveNum = 7000, data = None):
+
+    def get_sample(self, positive_num=7000):
+        data = self.df.copy()
         posindex = data[(data['sentiment'] == 'positive')].index.tolist()
         neuindex = data[(data['sentiment'] == 'neutral')].index.tolist()
         negindex = data[(data['sentiment'] == 'negative')].index.tolist()
-        posidx = np.random.choice(posindex,positveNum, replace = False).tolist()
+        posidx = np.random.choice(posindex, positive_num, replace=False).tolist()
 
-        return self.df.iloc[posidx + negindex + neuindex]
-    
+        self.df = data.iloc[posidx + negindex + neuindex].sample(frac=1)
+
     def cleaning(self, lowercase=True, remove_special=True, stemming=True, stop_words=None):
         '''
+
         :param lowercase:
         :param remove_special:
         :param stemming:
         :param stop_words:
-        :return: cleaned tokenized data with sentiment values (dataframe)
+        :return:
         '''
         print('Preprocessing the data')
         if not stop_words:
@@ -83,18 +86,10 @@ class Preprocessing:
         self.df['tokens'] = train_data
 
         return self.df
-    
 
 
 if __name__ == '__main__':
-    csv_file = 'reviews.csv'
-    preprocess = preprocess_csv(csv_file)
-    cleaneddata = preprocess.cleaning(stop_words='stopwords.txt')
-    samples = preprocess.getsample(data=cleaneddata).reset_index()
-    print(samples.head())
-
-
-
-
-
-
+    csv_file = 'data/reviews.csv'
+    preprocess = Preprocessing(csv_file, positive_num=5000)
+    df = preprocess.cleaning(stop_words='stopwords.txt')
+    print(df.shape[0])
